1e4a754d9c5e921e92e18295cf83a9c31245d1c1
giuliamaia2 <giuliamaia2@gmail.com>
2020-03-30 22:39:51 -0300

    menu horizontal

--- my-app/src/views/home.js
+++ my-app/src/views/home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Button from './../components/Button'
+import Menu from './../components/Menu'
 
 class Home extends React.Component{
 
     render(){
         return(
             <div>
-                <Button href='#/login' label='CADASTRAR'></Button>
+               <Menu />
             </div>
         )
     }
