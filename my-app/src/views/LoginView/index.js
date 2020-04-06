86bc57888788d3f01d33c83c584bd2ae6174b6ce
giuliamaia2 <giuliamaia2@gmail.com>
2020-04-06 18:18:56 -0300

    configurações de login

--- my-app/src/views/LoginView/index.js
+++ my-app/src/views/LoginView/index.js
@@ -3,15 +3,40 @@ import Menu from '../../components/Menu'
 import LogoImg from './../../assets/logo72.png'
 import './../../custom.css'
 import './style.css'
+import UsuarioService from './../../../service/userService'
+import  utils from './../../utils/index'
+import LocalStorage from './../../app/service/localStorage'
 
 class Login extends React.Component {
 
+    state = {
+        email: '',
+        senha: '',
+        mensagemErro: null
+    }
+
+    constructor() {
+        super();
+        this.service = new UsuarioService();
+    }
+
+    entrar = () => {
+        this.service.Login({
+            user: this.state.email,
+            password: utils.encrypt(this.state.senha)
+        }).then(response => {
+            localStorage.addItem('_usuario_logado', response.data);
+            //this.props.history.push(/homeUsuario);
+        }).catch(erro => {
+            this.setState({ mensagemErro: erro.response.data })
+        })
+    }
 
     render() {
         return (
             <>
                 <Menu />
-                    
+
                 <div id='login-container'>
 
 
@@ -25,12 +50,19 @@ class Login extends React.Component {
                     <form className='content'>
                         <p class='login-input'>
                             <label>Email:</label>
-                            <input type="text" />
+                            <input
+                                type="text"
+                                value={this.state.email}
+                                onChange={e => this.setState({ email: e.target.value })}
+                            />
                         </p>
                         <br />
                         <p class='login-input'>
                             <label>Senha:</label>
-                            <input type="password" />
+                            <input
+                                type="password"
+                                value={this.state.senha}
+                                onChange={e => this.setState({ senha: e.target.value })} />
                             <a href='' >Esqueci minha senha</a>
                         </p>
                         <br />
