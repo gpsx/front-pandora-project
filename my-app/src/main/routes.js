d5568370ae3e5741129923b5c31c639ce308910a
NicolleCS <nick.cruz123@hotmail.com>
2020-04-19 20:54:34 -0300

    Ajustes tela de cadastro

--- my-app/src/main/routes.js
+++ my-app/src/main/routes.js
@@ -5,7 +5,7 @@ import Cadastro from '../views/RegisterView/Register/index'
 import Servico from '../views/RegisterView/ServiceRegister/serviceRegister'
 import Home from './../views/HomeView/index.js'
 import Materialize from './../views/MateriaView'
-import DetailedExpansionPanel from '../views/RegisterView/ServiceRegister/Panel.js'
+import LimitTags from '../views/RegisterView/ServiceRegister/Panel.js'
 
 function Routes() {
     return (
@@ -14,7 +14,7 @@ function Routes() {
                 <Route path="/materialize" component={Materialize} />
                 <Route path="/login" component={Login} />
                 <Route path="/register" component={Cadastro} />
-                <Route path="/register" component={DetailedExpansionPanel} />
+                <Route path="/prestador" component={LimitTags} />
                 <Route path="/first-service" component={Servico}/>
                 <Route path="/" component={Home} />
             </Switch>
