87f865397593c4ff83f9e18a1cae4fa7f6079cb1
giuliamaia2 <giuliamaia2@gmail.com>
2020-04-20 15:42:13 -0300

    subindo pacotes

--- my-app/src/main/App.js
+++ my-app/src/main/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Router from './routes';
 import ReactNotification from 'react-notifications-component'
+import { store } from 'react-notifications-component';
+import 'react-notifications-component/dist/theme.css'
 import '../custom.css'
 
 function App() {
   return (
     <div>
       <Router />
+      <ReactNotification />
     </div>
   );
 }
