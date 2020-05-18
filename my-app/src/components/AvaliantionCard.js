40bd5f93a1337ffd7c1d6b47f7e5d3614e0a35a9
Guilherme de Paula Santos <guilherme.paula@easynvest.com.br>
2020-05-18 17:40:06 -0300

    adicona card de avaliação

---
+++ my-app/src/components/AvaliantionCard.js
@@ -0,0 +1,89 @@
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography, Card, CardContent } from "@material-ui/core";
+import { CardMedia, Divider, Box, Avatar } from "@material-ui/core";
+import Star from '@material-ui/icons/Star';
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex"
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)"
+  },
+  title: {
+    fontSize: 12,
+    margin: 0
+  },
+  pos: {
+    marginBottom: 12
+  },
+  cover: {
+    width: 150,
+    margin: 10,
+    borderRadius: "5%"
+  },
+  button: {
+    alignSelf: "flex-end",
+    width: "100%",
+    margin: 10,
+    backgroundColor: '#0B3C5D',
+    '&:hover': {
+      backgroundColor: '#328CC1',
+    },
+  },
+  divider: {
+    margin: 10
+  },
+  avatar: {
+    height: 15,
+    width: 15,
+    alignSelf: "flex-center",
+    margin: 3
+  },
+  star: {
+    color: "#FFD700",
+    height: 20
+  }
+});
+
+export default function ServiceCard(props) {
+  const classes = useStyles();
+  return (
+    <div>
+      <Card className={classes.root}>
+        <CardMedia
+          component="img"
+          className={classes.cover}
+          alt={props.name}
+          image={props.img}
+          title={props.name}
+        />
+        <Divider orientation="vertical" flexItem className={classes.divider} />
+        <Box flexGrow={1}>
+          <CardContent>
+            <Box className={classes.root} alignItems="center">
+              <Typography variant="h5" component="h3">
+                {props.name}
+              </Typography>
+            </Box>
+            <Box className={classes.root} alignItems="center">
+              <Typography variant="body2" component="h6">
+                Nota:
+              </Typography>
+              <Star className={classes.star} />
+              <Typography variant="body2" component="p" className={classes.star}>
+                {/* {props.avaliation} */}
+                4.7
+              </Typography>
+            </Box>
+            <Typography variant="body2" color="textSecondary" gutterBottom>
+                {props.avaliationText}
+            </Typography>
+          </CardContent>
+        </Box>
+      </Card>
+    </div>
+  );
+}
